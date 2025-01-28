4304a48a1b59c72a1c20d9405e9974e808c7548d
dvetterSnap <103961937+dvetterSnap@users.noreply.github.com>
2025-01-27 20:33:02 -0500

    Update 2_Class_Code_Analysis.py

--- pages/2_Class_Code_Analysis.py
+++ pages/2_Class_Code_Analysis.py
@@ -4,11 +4,11 @@ import time
 import os
 
 # Load environment variables using os
-URL = os.getenv("SL_CLASS_CODE_TASK_URL", "https://demo-fm.snaplogic.io/api/1/rest/feed-master/queue/ConnectFasterInc/Dylan%20Vetter/00_Embroker_Underwriting/output%20Task")
-BEARER_TOKEN = os.getenv("SL_CLASS_CODE_TASK_TOKEN", "12345")
+URL = os.getenv("SL_CRM_SQL_TASK_URL", "https://demo-fm.snaplogic.io/api/1/rest/feed-master/queue/ConnectFasterInc/Dylan%20Vetter/00_Embroker_Underwriting/output%20Task")
+BEARER_TOKEN = os.getenv("SL_CRM_SQL_TASK_TOKEN", "12345")
 timeout = int(os.getenv("SL_TASK_TIMEOUT", "1000"))
-page_title = os.getenv("CLASS_CODE_PAGE_TITLE", "Class Analysis")
-title = os.getenv("CLASS_CODE_TITLE", "Class Analysis")
+page_title = os.getenv("CRM_SQL_PAGE_TITLE", "CRM Agent")
+title = os.getenv("CRM_SQL_TITLE", "CRM Agent")
 
 # Streamlit Page Properties
 def typewriter(text: str, speed: int):
@@ -24,18 +24,20 @@ st.title(title)
 
 st.markdown(
     """  
-    ### This is a Agent that allows employees submit a file for class recommendation
+    ### This is a CRM Agent that allows employees to interact with Production Systems using Natural Language
     Example Questions
-    - Datashapes.pdf
+    - What accounts are in New York?
+    - What campaigns are completed and what were their performance metrics? Include names 
+    - What are my 3 top opportunities? Please include information about the respective account
  """
 )
 
 # Initialize chat history
-if "CLASS_CODE_messages" not in st.session_state:
-    st.session_state.CLASS_CODE_messages = []
+if "CRM_SQL_messages" not in st.session_state:
+    st.session_state.CRM_SQL_messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.CLASS_CODE_messages:
+for message in st.session_state.CRM_SQL_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -44,7 +46,7 @@ prompt = st.chat_input("Ask me anything")
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.CLASS_CODE_messages.append({"role": "user", "content": prompt})
+    st.session_state.CRM_SQL_messages.append({"role": "user", "content": prompt})
     with st.spinner("Working..."):
         data = {"prompt": prompt}
         headers = {
@@ -61,12 +63,12 @@ if prompt:
         if response.status_code == 200:
             result = response.json()
             if 'choices' in result:
-                response = result['choices'][0]['message']['content']
+                response = result['choices'][0]['message']['content'].replace("NEWLINE ", "**") + "**" + "\n\n"
                 # Display assistant response in chat message container
                 with st.chat_message("assistant"):
                     typewriter(text=response, speed=35)
                 # Add assistant response to chat history
-                st.session_state.CLASS_CODE_messages.append({"role": "assistant", "content": response})
+                st.session_state.CRM_SQL_messages.append({"role": "assistant", "content": response})
             else:
                 with st.chat_message("assistant"):
                     st.error(f"❌ Error in the SnapLogic API response")
